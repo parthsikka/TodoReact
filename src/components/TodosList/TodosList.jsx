6db955f3bf0256081073cd564d4ff66750ca6141
Parth Sikka <parthsikkah704@gmail.com>
2021-06-13 20:04:47 +0530

    React Todo App

---
+++ src/components/TodosList/TodosList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class TodosList extends Component {
+    state = {  }
+    render() { 
+       return( <h1> Hello from TodosList!! </h1>) ;
+    }
+}
+ 
+export default TodosList;
